4ed31b22182b50e056d234c46cb1fa3d2e37c92c
Kevin Christopher <kevinchristopherkc7@gmail.com>
2020-07-14 12:45:06 +0530

    Added the header to the file using awk

--- check_logs.sh
+++ check_logs.sh
@@ -5,6 +5,7 @@
 USER="$(id -un)"
 INPUTS="${#}"
 LOG_FILE="${1}" #File is in local directory
+OUTPUT_FILE='output.csv'
 
 # This function first fetches the matching Failed attempt using GREP command in a file
 # Retrieves the IP address from the matched lines using AWK
@@ -13,9 +14,13 @@ LOG_FILE="${1}" #File is in local directory
 # Checks the condition based on count using AWK
 # Output the details to a CSV file
 scan_log(){
+
+    echo 'Scanning the Log File'  
     grep 'Failed password' syslog-sample2 | awk '{print $11}' | sort -n | uniq -c |
-    awk '{ if ($1 >= 1) print $1 "," $2}' >> output.csv
+    #awk 'ip=$2 { if ($1 >= 1) print $1 " " $2 " " system("geoiplookup " ip);}' > output.csv |
+    awk 'BEGIN{print "COUNT""\tIP""\tLOCATION"}; ip=$2{ if ($1 >= 1) print $1 " " $2 " " system("geoiplookup " ip);}' > ${OUTPUT_FILE}
     echo 'Scanning Completed'
+
 }
 
 # Condition to check whether the User is Privileged User
@@ -24,7 +29,6 @@ then
     LOG_FILE='syslog-sample'
     if [[ "${INPUTS}" -eq 1 ]];
     then
-        echo 'Scanning the Log File'
         scan_log
     else
         echo "${LOG_FILE} is not provided"
@@ -34,3 +38,5 @@ else
     echo "${USER} You need root access to execute the script"
     exit 1
 fi
+
+exit 0
